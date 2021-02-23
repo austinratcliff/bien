88b62ea163efd0678484b3fbd9de1bd93d011d7f
austinratcliff <hello@austinratcliff.com>
2021-02-22 21:41:22 -0600

    Add reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,42 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(form_params)
+    @review.save
+
+    redirect_to root_path
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(form_params)
+
+    redirect_to review_path(@review)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    redirect_to root_path
+  end
+
+  def form_params
+    params.require(:review).permit(:title, :body, :score)
+  end
+end
