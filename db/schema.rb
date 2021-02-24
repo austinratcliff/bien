ce632b4f473eef3c14311adef29ca7baabb0f681
austinratcliff <hello@austinratcliff.com>
2021-02-24 09:54:59 -0600

    Add phone number and ambiance columns to reviews, and update review form and show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_222_155_338) do
+ActiveRecord::Schema.define(version: 20_210_224_012_428) do
   create_table 'reviews', force: :cascade do |t|
     t.string 'title'
     t.text 'body'
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20_210_222_155_338) do
     t.string 'cuisine'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.string 'phone_number'
+    t.string 'ambiance'
   end
 end
